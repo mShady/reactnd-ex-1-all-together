a9d296982c573c8407aba63097f7c0c4ffe9084f
Shady M. Najib <smostafa@integrant.com>
2019-04-15 01:45:00 +0200

    Adds CreateUser controlled component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import CreateUser from "./CreateUser";
 
 /*
 This exercise will help you put together and practice all of the concepts you've
@@ -11,6 +12,10 @@ The instructions for this project are located in the `instructions.md` file.
 */
 
 class App extends Component {
+  addUser(newUser) {
+    console.log(newUser);
+  }
+
   render() {
     return (
       <div className="App">
@@ -18,6 +23,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
+        <CreateUser onAddUser={newUser => this.addUser(newUser)} users={[]} />
       </div>
     );
   }
---
+++ src/CreateUser.js
@@ -0,0 +1,102 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class CreateUser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newUser: {
+        firstName: "",
+        lastName: "",
+        username: ""
+      }
+    };
+  }
+
+  static propTypes = {
+    users: PropTypes.array.isRequired,
+    onAddUser: PropTypes.func.isRequired
+  };
+
+  isDuplicateUserErrorShown() {
+    const { username } = this.state.newUser;
+    return this.userAlreadyExists(username);
+  }
+
+  userAlreadyExists(username) {
+    return this.props.users.includes(
+      user => user.username.toUpperCase() === username.toUpperCase()
+    );
+  }
+
+  isAddButtonDisabled() {
+    const { firstName, lastName, username } = this.state.newUser;
+
+    if (this.isDuplicateUserErrorShown()) return true;
+    if (firstName === "" || lastName === "" || username === "") {
+      return true;
+    }
+    return false;
+  }
+
+  addUser() {
+    this.props.onAddUser(this.state.newUser);
+    this.setState({
+      newUser: {
+        firstName: "",
+        lastName: "",
+        username: ""
+      }
+    });
+  }
+
+  onChange(event) {
+    const { id, value } = event.target;
+    console.log(value);
+    console.log(id);
+    this.setState(currentState => ({
+      newUser: { ...currentState.newUser, [id]: value }
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          id="firstName"
+          type="text"
+          placeholder="First Name"
+          value={this.state.newUser.firstName}
+          onChange={event => this.onChange(event)}
+        />
+        <input
+          id="lastName"
+          type="text"
+          placeholder="Last Name"
+          value={this.state.newUser.lastName}
+          onChange={event => this.onChange(event)}
+        />
+        <input
+          id="username"
+          type="text"
+          placeholder="Username"
+          value={this.state.newUser.username}
+          onChange={event => this.onChange(event)}
+        />
+        <button
+          disabled={this.isAddButtonDisabled()}
+          onClick={() => this.addUser()}
+        >
+          Add
+        </button>
+        {this.isDuplicateUserErrorShown() ? (
+          <p className="error">Error: Duplicate username!</p>
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  }
+}
+
+export default CreateUser;
